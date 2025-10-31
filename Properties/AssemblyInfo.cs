5d809df9b80f8d52e13561e9c81810cb5a20b4bf
kalantariamin1369-ux <kalantari.amin1369@gmail.com>
2025-10-31 23:09:58 +0330

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Equipment Tracker")]
+[assembly: AssemblyDescription("Advanced Equipment Tracking System with Authentication, Reporting, and Asset Management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Equipment Tracker Inc.")]
+[assembly: AssemblyProduct("Equipment Tracker Desktop")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1b2c3d4-e5f6-4a5b-8c7d-9e0f1a2b3c4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
