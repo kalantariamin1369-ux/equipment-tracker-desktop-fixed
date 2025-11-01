f6c17c7e804505ba67b6289039230e340bc5ff71
kalantariamin1369-ux <kalantari.amin1369@gmail.com>
2025-11-01 05:42:34 +0330

    Add missing EquipmentForm to unblock build.

--- Views/EquipmentForm.cs
+++ Views/EquipmentForm.cs
@@ -1,12 +1,22 @@
 using System.Windows.Forms;
+using EquipmentTracker.Models;
 
 namespace EquipmentTracker.Views
 {
     public partial class EquipmentForm : Form
     {
+        public Equipment Equipment { get; set; }
+
         public EquipmentForm()
         {
             InitializeComponent();
+            Equipment = new Equipment();
+        }
+
+        public EquipmentForm(Equipment equipment)
+        {
+            InitializeComponent();
+            Equipment = equipment;
         }
     }
 }
